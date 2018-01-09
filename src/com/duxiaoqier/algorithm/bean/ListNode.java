6575b928ad4a025497719aba3e23638dc0257a09
dupeng <1098403264@qq.com>
2018-01-09 10:08:22 +0800

    合并有序链表

--- src/com/duxiaoqier/algorithm/bean/ListNode.java
+++ src/com/duxiaoqier/algorithm/bean/ListNode.java
@@ -1,10 +1,56 @@
 package com.duxiaoqier.algorithm.bean;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class ListNode {
     public int val;
     public ListNode next = null;
 
     public ListNode(int val) {
         this.val = val;
+    }
+
+/*    public ListNode(int[] vals){
+        if (vals.length > 1){
+            this.val = vals[0];
+            int[] dest = Arrays.copyOfRange(vals, 1, vals.length);
+            this.next = new ListNode(dest);
+        } else {
+            this.val = vals[0];
+            this.next = null;
+        }
+    }*/
+
+    public ListNode(int[] vals) {
+        if (vals.length == 0) return;
+        this.val = vals[0];
+        ListNode temp = this;
+        for (int i = 1; i < vals.length; i++) {
+            temp.next = new ListNode(vals[i]);
+            temp = temp.next;
+        }
+    }
+
+
+    public void print() {
+        ListNode head = this;
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+
+    @Override
+    public String toString() {
+        List<Integer> list = new ArrayList<>();
+        ListNode head = this;
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        return list.toString();
     }
 }
---
+++ src/com/duxiaoqier/algorithm/list/merge/Solution.java
@@ -0,0 +1,62 @@
+package com.duxiaoqier.algorithm.list.merge;
+
+import com.duxiaoqier.algorithm.bean.ListNode;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(new int[]{1, 3, 5});
+        ListNode listNode2 = new ListNode(new int[]{2, 4, 6});
+
+        new Solution().Merge(listNode1, listNode2).print();
+        new Solution().Merge2(listNode1, listNode2).print();
+    }
+    public ListNode Merge(ListNode list1, ListNode list2) {
+        if(list1 == null){
+            return list2;
+        }
+        if (list2 == null){
+            return  list1;
+        }
+        ListNode min = list1.val < list2.val ? list1: list2;
+        ListNode max = list1.val >= list2.val ? list1: list2;
+
+        ListNode result = min;
+        ListNode temp = min;
+
+        while (min.next != null){
+            ListNode minNext = min.next;
+            if (minNext.val <= max.val){
+                temp.next = minNext;
+                min = min.next;
+            } else {
+                min = max;
+                temp.next = max;
+                max = minNext;
+            }
+            temp = temp.next;
+        }
+        temp.next = max;
+        return result;
+    }
+
+    public ListNode Merge2(ListNode list1, ListNode list2) {
+        if(list1 == null){
+            return list2;
+        }
+        if (list2 == null){
+            return  list1;
+        }
+
+        ListNode head;
+        if (list1.val < list2.val){
+            head = list1;
+            head.next = Merge2(list1.next, list2);
+        } else {
+            head = list2;
+            head.next = Merge2(list1, list2.next);
+        }
+        return head;
+    }
+
+}
